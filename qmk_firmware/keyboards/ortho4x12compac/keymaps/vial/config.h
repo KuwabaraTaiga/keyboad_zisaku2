4086b9a86c19c977a83ad81a59be9ea35c1d6005
KuwabaraTaiga <kwbrtaiga@gmail.com>
2025-11-02 19:26:51 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/ortho4x12compac/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0b, 0xf9, 0x14, 0x9c, 0x5e, 0x38, 0xfd, 0xf1}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
